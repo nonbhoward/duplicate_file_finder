23ae7edfa18dfd8358dcbd39dd4dc721af8336b7
nonbhoward <55259160+nonbhoward@users.noreply.github.com>
2023-07-09 16:42:55 -0400

    Add early path check for settings
    Add description for BUF_SIZE key

--- settings.py
+++ settings.py
@@ -2,12 +2,15 @@
 
 # imports, python
 from os import environ
+from os.path import exists
+from pathlib import Path
 
 home_drive = environ.get("HOMEDRIVE")
 if not home_drive:
     raise OSError(f'Could not locate home_drive : {home_drive}')
 
 # VARIABLES AND EXPLANATION
+#   BUF_SIZE: for hash generation, limits RAM usage when hashing large files
 #   home_drive: the primary hard drive, probably C:\
 #   debug: set to True to enable developer mode, no files will be moved in
 #       this mode, only copied
@@ -20,14 +23,25 @@ settings = {
     'BUF_SIZE': 65536,
     'debug': True,
     'extensions': [
-        'iso',
+        #'mp4',
     ],
     'file_source': [
-        f'{home_drive}',
-        'path_to_testing_source',
+        #f'{home_drive}',
+        'D:\\'
+        'mega',
     ],
     'file_destination': [
         f'{home_drive}',
         'path_to_testing_destination',
     ]
 }
+
+# Verify required folders exist
+file_source_elements = settings['file_source']
+file_destination_elements = settings['file_destination']
+path_to_source = str(Path('\\'.join(file_source_elements)))
+path_to_destination = str(Path('\\'.join(file_destination_elements)))
+if not exists(path_to_source):
+    print(f'Path does not exist : {path_to_source}')
+if not exists(path_to_destination):
+    print(f'Path does not exist : {path_to_destination}')
